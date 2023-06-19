0adc47fd1d20c0d4c431d0449a42fd57d8bfeecf
Leonardo Barbosa <128062428+leleo23@users.noreply.github.com>
2023-06-19 20:04:21 -0300

    Add files via upload

---
+++ AppGBLZ/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Menu from './components/Menu';
+import LoginForm from './components/LoginForm';
+
+function App() {
+  return (
+    <div>
+      <Menu />
+      <main>
+        <LoginForm />
+      </main>
+    </div>
+  );
+}
+
+export default App;
